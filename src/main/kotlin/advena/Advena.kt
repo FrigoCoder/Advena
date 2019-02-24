7c87af432b95d6c90f4f469852bad2cef3648470
Frigo <frigocoder@gmail.com>
2019-02-25 00:26:44 +0100

    Fuck GLSL ShaderToy incompatibility.

--- src/main/kotlin/advena/Advena.kt
+++ src/main/kotlin/advena/Advena.kt
@@ -3,41 +3,51 @@ package advena
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Mesh
+import com.badlogic.gdx.graphics.VertexAttribute
 import com.badlogic.gdx.graphics.glutils.ShaderProgram
 import org.lwjgl.opengl.GL20
-import com.badlogic.gdx.graphics.VertexAttribute
 
-
-
-class Advena : ApplicationAdapter(){
+class Advena : ApplicationAdapter() {
 
     private lateinit var shader: ShaderProgram
     private lateinit var mesh: Mesh
+    private var startTime: Long = 0
+    private var width: Int = 0
+    private var height: Int = 0
 
     override fun create() {
-        val vertexShader = Gdx.files.internal("vertex.glsl").readString()
-        val fragmentShader = Gdx.files.internal("fragment.glsl").readString()
-        shader = ShaderProgram(vertexShader, fragmentShader)
+        shader = ShaderProgram(Gdx.files.internal("vertex.glsl"), Gdx.files.internal("fragment.glsl"))
+        if (!shader.isCompiled) {
+            System.err.println(shader.log)
+            System.exit(-1)
+        }
+
         mesh = Mesh(true, 4, 6, VertexAttribute.Position(), VertexAttribute.ColorUnpacked(), VertexAttribute
             .TexCoords(0))
-        mesh.setVertices(floatArrayOf(-0.5f, -0.5f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 0.5f, -0.5f, 0f, 1f, 1f, 1f, 1f, 1f,
-            1f, 0.5f, 0.5f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, -0.5f, 0.5f, 0f, 1f, 1f, 1f, 1f, 0f, 0f))
+        mesh.setVertices(floatArrayOf(
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            -1.0f, 1.0f, 0.0f))
         mesh.setIndices(shortArrayOf(0, 1, 2, 2, 3, 0))
-    }
 
-    override fun dispose() {
+        startTime = System.currentTimeMillis()
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         shader.begin()
-//        shader.setUniformMatrix("u_projTrans", matrix)
-//        shader.setUniformi("u_texture", 0)
+        //shader.setUniformf("iTime", (System.currentTimeMillis() - startTime) / 1000f)
+//        shader.setUniformf("iResolution", Vector2(width.toFloat(), height.toFloat()))
         mesh.render(shader, GL20.GL_TRIANGLES)
-        shader.end();
+        shader.end()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        this.width = width;
+        this.height = height
     }
 
 }
-
--- src/main/kotlin/advena/DesktopLauncher.kt
+++ src/main/kotlin/advena/DesktopLauncher.kt
@@ -6,5 +6,6 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 fun main(args: Array<String>) {
     val config = Lwjgl3ApplicationConfiguration()
     config.setTitle("Advena")
+    config.useOpenGL3(true, 3, 2)
     Lwjgl3Application(Advena(), config)
 }
